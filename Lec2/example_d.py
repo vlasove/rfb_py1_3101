b706c5b7b6826066d74080a76f377fa3bd2734de
EvgenyVl <evgeny_vlasov@yahoo.com>
2022-01-31 13:14:04 +0300

    Лекция 2 : задача E

---
+++ Lec2/example_d.py
@@ -0,0 +1,8 @@
+# Решение задачи E
+drink = "cola"
+meal = "burger and cheese"
+subMeal = "carrot"
+time = "right now"
+
+phrase = f"I wanna some '{drink}', my favorite meal - '{meal}'. Give me also '{subMeal}'. I will come soon... '{time}'"
+print(phrase)
