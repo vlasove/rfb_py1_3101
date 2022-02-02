a6b296b08631b31ea77adb08ac82473e9c8a4e9b
EvgenyVl <evgeny_vlasov@yahoo.com>
2022-02-02 11:13:21 +0300

    Лекция 3: строки

---
+++ Lec3/example_a.py
@@ -0,0 +1,21 @@
+# Целые числа
+a_int = 5
+b_int = 3
+
+print(f"A value {a_int} and B value {b_int}")
+print("Type of A:", type(a_int))
+print(a_int, b_int)
+print("Values:", a_int, b_int)
+
+# Арифметика
+print("a_int + b_int = ", a_int + b_int)
+print("a_int - b_int = ", a_int - b_int)
+print("a_int * b_int = ", a_int * b_int)
+# Нюанс при делении - всегда вещественное число!!!
+print("a_int / b_int = ", a_int / b_int)
+# Классическое целочисленное деление - это //
+print("a_int // b_int = ", a_int // b_int)
+# Взятие остатка от деления
+print("a_int % b_int = ", a_int % b_int)
+# Возведение в степень
+print("a_int ^ b_int = ", a_int ** b_int)
