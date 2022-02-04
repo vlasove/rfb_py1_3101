fee98fe3c571ede7850d2a5bd66c35bdef8c1f5f
EvgenyVl <evgeny_vlasov@yahoo.com>
2022-02-04 11:54:17 +0300

    Лекция 4: условный оператор

---
+++ Lec4/example_f.py
@@ -0,0 +1,10 @@
+# Решение задачи Q
+a_float = float(input())
+b_float = float(input())
+
+result = a_float + b_float # 8.0, 9.0, 11435.0
+
+if result % 2 ==0: # 8.0 % 2 == 0 => 0.0 == 0 => 0.0 == 0.0
+    print("ЧЁТНОЕ")
+else:
+    print("НЕЧЁТНОЕ")
